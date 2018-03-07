c6129a69a15e68f897c344831cd4cc18545c384d
mjharrison <mjsonofharry@gmail.com>
2018-03-07 10:37:50 -0500

    wip

--- loader.py
+++ loader.py
@@ -1,25 +1,40 @@
 import os
+import numpy as np
 import shutil
 
 # Reference: https://github.com/sorki/python-mnist/blob/master/mnist/loader.py
 class ReplayLoader:
-    def __init__(self, dataset_apath='.'):
+    def __init__(self):
         self.dataset_apath = dataset_apath
+        self.sets_dname = 'sets'
         self.testing_dname = 'testing'
         self.training_dname = 'training'
 
-        self.testing_x = []
-        self.testing_y = []
+        self.testing_frames = []
+        self.testing_labels = []
 
-        self.training_x = []
-        self.training_y = []
+        self.training_frames = []
+        self.training_labels = []
 
-    def load(self, x_apath, y_apath):
-        pass
+    def load(self, frames_apath, labels_apath):
+        frames = []
+        for dirent in os.listdir(frames_apath):
+            dirent_apath = os.path.join(frames_apath, dirent)
+            frames.append(np.load(dirent_apath))
+        labels = []
+        for dirent in os.listdir(labels_apath):
+            dirent_apath = os.path.join(frames_apath, dirent)
+            labels.append(np.load(dirent_apath))
+        return (frames, labels)
 
     def load_testing(self):
-        return self.testing_x, self.testing_y
+        set_apath = os.path.join(self.sets_dname, self.testing_dname)
+        frames_apath = os.path.join(set_apath, 'frames')
+        labels_apath = os.path.join(set_apath, 'labels')
+        self.testing_frames, self.testing_labels = self.load(frames_apath, labels_apath)
 
     def load_training(self):
-        return self.training_x, self.training_y
-
+        set_apath = os.path.join(self.sets_dname, self.training_dname)
+        frames_apath = os.path.join(set_apath, 'frames')
+        labels_apath = os.path.join(set_apath, 'labels')
+        self.training_frames, self.training_labels = self.load(frames_apath, labels_apath)
