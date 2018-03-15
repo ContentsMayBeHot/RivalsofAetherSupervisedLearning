fd1ff8242221c2ac5e12b18023f53e542965d1ee
mjharrison <mjsonofharry@gmail.com>
2018-03-14 20:58:57 -0400

    loads lists of paths to x and y files. can get x,y matrices in batches

--- loader.py
+++ loader.py
@@ -1,3 +1,4 @@
+import configparser
 import os
 import numpy as np
 import shutil
@@ -5,36 +6,72 @@ import shutil
 # Reference: https://github.com/sorki/python-mnist/blob/master/mnist/loader.py
 class ReplayLoader:
     def __init__(self):
-        self.dataset_apath = dataset_apath
-        self.sets_dname = 'sets'
-        self.testing_dname = 'testing'
-        self.training_dname = 'training'
+        self.config = configparser.ConfigParser()
+        self.config.read('config.ini')
 
-        self.testing_frames = []
-        self.testing_labels = []
+        self.x_train = []
+        self.y_train = []
+        self.x_test = []
+        self.y_test = []
 
-        self.training_frames = []
-        self.training_labels = []
+    def load(self, set_apath):
+        print('Loading from', set_apath)
 
-    def load(self, frames_apath, labels_apath):
-        frames = []
-        for dirent in os.listdir(frames_apath):
-            dirent_apath = os.path.join(frames_apath, dirent)
-            frames.append(np.load(dirent_apath))
-        labels = []
-        for dirent in os.listdir(labels_apath):
-            dirent_apath = os.path.join(frames_apath, dirent)
-            labels.append(np.load(dirent_apath))
-        return (frames, labels)
+        batch_x = []
+        print('Loading frames...')
+        xset_apath = os.path.join(set_apath, 'frames')
+        for xdir_dname in self.__listdir_subdir_only(xset_apath):
+            xdir_apath = os.path.join(xset_apath, xdir_dname)
+            for x_fname in self.__listdir_np_only__(xdir_apath):
+                x_apath = os.path.join(xdir_apath, x_fname)
+                batch_x.append(x_apath)
 
-    def load_testing(self):
-        set_apath = os.path.join(self.sets_dname, self.testing_dname)
-        frames_apath = os.path.join(set_apath, 'frames')
-        labels_apath = os.path.join(set_apath, 'labels')
-        self.testing_frames, self.testing_labels = self.load(frames_apath, labels_apath)
+        batch_y = []
+        print('Loading labels...')
+        yset_apath = os.path.join(set_apath, 'labels')
+        for ydir_dname in self.__listdir_subdir_only(yset_apath):
+            ydir_apath = os.path.join(yset_apath, ydir_dname)
+            for y_fname in self.__listdir_np_only__(ydir_apath):
+                y_apath = os.path.join(ydir_apath, y_fname)
+                batch_y.append(y_apath)
+
+        return (batch_x,batch_y)
 
     def load_training(self):
-        set_apath = os.path.join(self.sets_dname, self.training_dname)
-        frames_apath = os.path.join(set_apath, 'frames')
-        labels_apath = os.path.join(set_apath, 'labels')
-        self.training_frames, self.training_labels = self.load(frames_apath, labels_apath)
+        set_apath = self.config['SETS']['PathToTraining']
+        (self.x_train,self.y_train) = self.load(set_apath)
+        return (self.x_train,self.y_train)
+
+    def load_testing(self):
+        set_apath = self.config['SETS']['PathToTesting']
+        (self.x_test,self.y_test) = self.load(set_apath)
+        return (self.x_test,self.y_test)
+
+    def next_training(self, size=100):
+        x = []
+        y = []
+        for i in range(size):
+            x.append(np.load(self.x_train.pop()))
+            y.append(np.load(self.y_train.pop()))
+        return (x,y)
+
+    def next_testing(self, size=100):
+        x = []
+        y = []
+        for i in range(size):
+            x.append(np.load(self.x_test.pop()))
+            y.append(np.load(self.y_test.pop()))
+        return (x,y)
+
+    def __listdir_subdir_only(self, apath):
+        return [
+            dirent for dirent in os.listdir(apath)
+            if os.path.isdir(os.path.join(apath, dirent))
+        ]
+
+    def __listdir_np_only__(self, apath):
+        return [
+            dirent for dirent in os.listdir(apath)
+            if os.path.isfile(os.path.join(apath, dirent))
+            and (dirent.endswith('np') or dirent.endswith('npy'))
+            ]
