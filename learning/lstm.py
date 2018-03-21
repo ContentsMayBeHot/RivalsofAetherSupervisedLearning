2d07d890584a2786805f2788fc738ac6adee8fc6
mjharrison <mjsonofharry@gmail.com>
2018-03-21 13:05:51 -0400

    working lstm/convnet model, but need to handle timesteps value in input_shape

--- learning/lstm.py
+++ learning/lstm.py
@@ -3,7 +3,7 @@ import numpy as np
 import keras
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Embedding, LSTM, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Embedding, TimeDistributed, LSTM, Flatten, Conv2D, MaxPooling2D
 
 import loader
 
@@ -14,8 +14,12 @@ def main():
     # Display device information
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
     # Data attributes
-    input_shape = (135, 240, 1)
-    classes = 9
+    nb_classes = 9
+    nb_timesteps = None  # TODO: Specify this
+    img_height = 135
+    img_width = 240
+    img_colors = 1
+    input_shape = (nb_timesteps, img_height, img_width, img_colors)
     # Output file
     model_fname = 'rival.h5'
     # Data flow control
@@ -24,48 +28,69 @@ def main():
     # Initialize loader
     roa = loader.ROALoader()
 
+    model = Sequential()
+
+    model.add(TimeDistributed(
+        Conv2D(8, kernel_size=(2, 2), activation='relu'),
+        input_shape=input_shape))
+    model.add(TimeDistributed(
+        Conv2D(8, kernel_size=(2, 2), activation='relu')))
+    model.add(TimeDistributed(
+        MaxPooling2D(pool_size=(2, 2))))
+    model.add(TimeDistributed(
+        Dropout(0.1)))
+    model.add(TimeDistributed(
+        Flatten()
+    ))
+    model.add(LSTM(4))
+    model.add(Dense(16))
+    model.add(Dense(nb_classes))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    model.summary()
+    quit()
+
+
     # Basic test
-    # model = Sequential()
     # model.add(Dense(units=32, input_shape=input_shape))
     # model.add(Dense(units=10))
     # model.add(Flatten())
-    # model.add(Dense(units=classes, activation='relu'))
-    # model.add(Dense(units=classes, activation='relu'))
-    # model.add(Dense(units=classes, activation='sigmoid'))
+    # model.add(Dense(units=nb_classes, activation='relu'))
+    # model.add(Dense(units=nb_classes, activation='relu'))
+    # model.add(Dense(units=nb_classes, activation='sigmoid'))
     # model.compile(optimizer='rmsprop',
     #               loss='categorical_crossentropy',
     #               metrics=['accuracy'])
 
     # LSTM test
-    # model = Sequential()
-    # model.add(LSTM(classes, input_shape=input_shape, return_sequences=True))
+    # model.add(LSTM(nb_classes, input_shape=input_shape, return_sequences=True))
     # model.add(Dropout(0.1))
-    # model.add(LSTM(classes, input_shape=input_shape, return_sequences=True))
+    # model.add(LSTM(nb_classes, input_shape=input_shape, return_sequences=True))
     # model.add(Dropout(0.1))
     # model.add(Flatten())
-    # model.add(Dense(classes, activation='softmax'))
+    # model.add(Dense(nb_classes, activation='softmax'))
     # model.compile(loss='categorical_crossentropy',
     #               optimizer='adam',
     #               metrics=['accuracy'])
 
     # Conv2D test
-    model = Sequential()
-    model.add(Conv2D(4,
-                     kernel_size=(3, 3),
-                     activation='relu',
-                     input_shape=input_shape))
-    model.add(Conv2D(8,
-                    kernel_size=(3, 3),
-                    activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(16, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(classes, activation='softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
+    #model.add(Conv2D(4,
+    #                 kernel_size=(3, 3),
+    #                 activation='relu',
+    #                 input_shape=input_shape))
+    #model.add(Conv2D(8,
+    #                kernel_size=(3, 3),
+    #                activation='relu'))
+    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(Dropout(0.25))
+    #model.add(Flatten())
+    #model.add(Dense(16, activation='relu'))
+    #model.add(Dropout(0.5))
+    #model.add(Dense(nb_classes, activation='softmax'))
+    #model.compile(loss='categorical_crossentropy',
+    #              optimizer='adam',
+    #              metrics=['accuracy'])
 
     # Train model
     roa.load_training_set()
