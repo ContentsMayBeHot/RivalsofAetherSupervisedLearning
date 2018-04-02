3e4f2587e87504588bedaab8f19aae2281494062
mjharrison <mjsonofharry@gmail.com>
2018-04-01 21:00:00 -0400

    more robust network, and testing loop (untested) code duplication present in two loops, need a generator or something new network model is doublestacked convo lstmeroo

--- learning/lstm.py
+++ learning/lstm.py
@@ -24,7 +24,7 @@ CLIP_Y_SHAPE = (CLIP_LENGTH, CLASSES)
 BATCH_X_SHAPE = (1, CLIP_LENGTH, IMG_U, IMG_V, IMG_C)
 BATCH_Y_SHAPE = (1, CLIP_LENGTH, CLASSES)
 
-FILTERS = 8
+FILTERS = 10
 POOL_SIZE = (1, 135, 240)
 KERNEL_SIZE = (3, 3)
 
@@ -48,6 +48,15 @@ def main():
             return_sequences=True,
             stateful=True))
     model.add(BatchNormalization())
+    model.add(ConvLSTM2D(
+            filters=FILTERS,
+            kernel_size=KERNEL_SIZE,
+            data_format='channels_last',
+            padding='same',
+            return_sequences=True,
+            stateful=True
+    ))
+    model.add(BatchNormalization())
     model.add(AveragePooling3D(POOL_SIZE))
     model.add(Reshape((-1, FILTERS)))
     model.add(Dense(CLASSES, activation='sigmoid'))
@@ -67,7 +76,7 @@ def main():
     for e in range(EPOCHS):
         print('Epoch: {}/{}'.format(e+1, EPOCHS))
         for i in range(n):
-            print('\tBatch: {}/{}'.format(i+1, n))
+            print('\tTraining batch: {}/{}'.format(i+1, n))
             # Get a single replay
             batch = roa_loader.next_training_batch()
             if not batch:
@@ -98,8 +107,34 @@ def main():
 
     # Test model
     testing_set_path = config['SETS']['PathToTesting']
-    roa_loader.load_testing_set(testing_set_path)
-    # TODO Testing loop
+    n = roa_loader.load_testing_set(testing_set_path)
+    for i in range(n):
+        print('Testing batch: {}/{}'.format(i+1, n))
+        batch = roa_loader.next_testing_batch()
+        if not batch:
+            break
+        batch_x, batch_y = batch
+        batch_x = np.array(batch_x, dtype=np.int32)
+        batch_y = np.array(batch_y, dtype=np.int32)
+        for j in range(0, batch_x.shape, CLIP_LENGTH):
+            x_clip = batch_x[j:j+CLIP_LENGTH]
+            y_clip = batch_y[j:j+CLIP_LENGTH]
+            # Check if clip is required length
+            if x_clip.shape[0] < CLIP_LENGTH:
+                continue
+            timesteps = batch_x.shape[0] // 100
+            print('\t\tClip: {}/{}'.format((j//100)+1, timesteps), end='\t')
+            x_clip = x_clip.reshape(BATCH_X_SHAPE)
+            y_clip = y_clip.reshape(BATCH_Y_SHAPE)
+            scalars = model.test_on_batch(x_clip, y_clip)
+            loss = scalars[0]
+            accuracy = scalars[1]
+            print('Loss: {0:.2f}'.format(loss), end='\t')
+            print('Accuracy: {0:.2f}'.format(accuracy))
+        # Reset LSTM for next video
+        model.reset_states()
+        print()
+    print()
 
     # Save model
     model_path = os.path.join('..', 'models', MODEL_FNAME)
