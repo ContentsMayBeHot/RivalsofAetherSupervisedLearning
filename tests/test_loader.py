1e295f56578ae327bba78427d42494da69715c4b
mjharrison <mjsonofharry@gmail.com>
2018-04-06 11:46:25 -0400

    shh

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,20 +1,27 @@
-from test_common import learning
-# from learning import ROALoader
-
+import os
 import pytest
 
+from test_common import learning
+from learning import loader
+
+SAMPLES_PATH = os.path.join('..', 'samples')
+SAMPLE_SET_PATH = os.path.join(SAMPLES_PATH, 'sample_set')
+SAMPLE_SET_SIZE = 10
+
+def test_init():
+    roa = loader.ROALoader()
+    assert roa.x_train == []
+    assert roa.y_train == []
+    assert roa.x_test == []
+    assert roa.y_test == []
+    assert roa.train_queue == None
+    assert roa.test_queue == None
 
 def test_load_set():
-    pytest.fail('Not implemented')
+    roa = loader.ROALoader()
+    n = roa.load_training_set(SAMPLE_SET_PATH, max_queue_size=1)
+    assert n == SAMPLE_SET_SIZE
 
-
-def test_unpack_sample():
-    pytest.fail('Not implemented')
-
-
-def test_next_batch():
-    pytest.fail('Not implemented')
-
-
-def test_get_count():
-    pytest.fail('Not implemented')
+def test_next_batch)(:
+    roa = loader.ROALoader()
+    roa.load_training_set(SAMPLE_SET_PATH, max_queue_size=1)
